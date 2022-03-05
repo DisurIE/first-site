c15beb0dc07ba6d00be15b585cdbd2a4c99edbd9
DisurIE <96732372+DisurIE@users.noreply.github.com>
2022-03-06 00:14:53 +0400

    tabs and destination adaptive

--- js/script.js
+++ js/script.js
@@ -37,8 +37,94 @@ function tabEvents(nameTab,tabs){
     });
     delegateTabsActive(nameTab, tabs, 'click');
 }
-tabEvents(navTabClass,navtabs);
-tabEvents(TabClass, tabs);
+//tabEvents(navTabClass,navtabs);
+//tabEvents(TabClass, tabs);
 tabEvents(sliderClass, sliders);
 tabEvents(bigSliderClass, bigSliders);
 
+//tabClass = tabs
+
+const        tabsContentIMG = document.querySelectorAll('.destination__planet'),
+             tabsContent = document.querySelectorAll('.destination__target'),
+             tabsParent = document.querySelector('.interactive__tabs');
+          
+
+
+        function hideTabContent(){
+            tabsContent.forEach(item =>{
+                item.classList.add('hidden');
+                
+            });
+            tabsContentIMG.forEach(item =>{
+                item.classList.add('hidden');
+                
+            });
+            tabs.forEach(item =>{
+                item.classList.remove('interactive__tab_active');
+            });
+        }   
+        function showTabContent(i = 0){
+            tabsContent[i].classList.remove('hidden');
+            tabsContentIMG[i].classList.remove('hidden');
+            tabs[i].classList.add('interactive__tab_active');
+        }
+
+        hideTabContent();
+        showTabContent();
+
+
+       
+        tabsParent.addEventListener('click', (event)=>{
+                const target = event.target;
+    
+                if(target && target.classList.contains('interactive__tab')){
+                    tabs.forEach((item, i) => {
+                        if(target == item){
+                            hideTabContent();
+                            showTabContent(i);
+
+                        }
+                        
+                    });
+                }
+            
+        });
+
+   const sasParents = document.querySelector('.home-header'),
+         sasContent = document.querySelectorAll('.page');
+
+//navtabs = tabs
+//navTabClass = tab
+
+     function hiddenContent(){
+            sasContent.forEach(item => {
+                item.classList.add('hidden');
+            });
+
+            navtabs.forEach(item => {
+                item.classList.remove('nav-interactive__tab_active');
+            });
+        }
+
+    function AddContent(i = 0){
+        sasContent[i].classList.remove('hidden');
+        navtabs[i].classList.add('nav-interactive__tab_active');
+    }
+        
+    hiddenContent();
+    AddContent();
+
+    sasParents.addEventListener('click', (event)=>{
+        const target = event.target;
+
+        if(target && target.classList.contains(navTabClass)){
+            navtabs.forEach((item, i) => {
+                if(target == item){
+                    hiddenContent();
+                    AddContent(i);
+                    console.log("sssssssssssssss");
+                }
+                
+            });
+        }
+    });
